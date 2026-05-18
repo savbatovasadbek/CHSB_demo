352b161e934658ae4cc8ba8f2a642fd6cbac38f5
savbatovasadbek <asavbatov@gmail.com>
2026-05-18 17:02:04 +0500

    4th CHSB questions updated

--- script.js
+++ script.js
@@ -66,103 +66,109 @@ mathQuestions = mathQuestions.sort(() => Math.random() - 0.5);
 const englishQuestions = [
   {
     type: "text",
-    question: "Which instrument is played by pressing keys on a keyboard?",
-    answers: "A) drum   B) cymbals   C) keyboard   D) flute",
-    answer: 2,
-  },
-  {
-    type: "text",
-    question: "Which of these is used to find information about many topics?",
-    answers: "A) letter   B) dictionary   C) encyclopedia   D) email",
-    answer: 2,
-  },
-  {
-    type: "text",
-    question: "Which of these is a renewable energy source?",
-    answers: "A) wind power   B) gas   C) coal   D) oil",
-    answer: 0,
-  },
-  {
-    type: "text",
-    question: "She sings very ___, so everyone likes her voice.",
-    answers: "A) beautiful   B) beauty   C) beautifully   D) beautify",
-    answer: 2,
-  },
-  {
-    type: "text",
-    question: "John plays the guitar ___ than his sister.",
-    answers: "A) good   B) well   C) better   D) the best",
-    answer: 2,
-  },
-  {
-    type: "text",
-    question: "When I was little, I ___ run very fast.",
-    answers: "A) can   B) could   C) may   D) might",
-    answer: 1,
-  },
-  {
-    type: "text",
-    question: "He ___ finish his homework yesterday because he was tired.",
-    answers: "A) couldn’t   B) could   C) can’t   D) can",
-    answer: 0,
-  },
-  {
-    type: "text",
-    question: "___ he ride a bicycle when he was five?",
-    answers: "A) Was   B) Can   C) Does   D) Could",
-    answer: 3,
-  },
-  {
-    type: "text",
-    question: "___ we close the windows to keep the room warm?",
-    answers: "A) Should   B) Can   C) Are   D) Did",
-    answer: 0,
-  },
-  {
-    type: "text",
-    question: "We should use ___ water when watering the plants.",
-    answers: "A) little   B) few   C) less   D) a few",
-    answer: 2,
-  },
-  {
-    type: "text",
-    question: "The city needs ___ water for the fields than before.",
-    answers: "A) most   B) more   C) much   D) many",
+    question: "Which astronaut was the first person to walk on the Moon?",
+    answers:
+      "A) Yuri Gagarin   B) Neil Armstrong   C) Thomas Edison   D) Albert Einstein",
     answer: 1,
   },
   {
     type: "text",
     question:
-      "To save nature, you should use _ water but _ sunlight to help the plants grow.",
-    answers: "A) less, more   B) more, less   C) many, few   D) little, many",
+      "During the carnival, people wore colorful costumes and danced in a huge ________.",
+    answers: "A) parade   B) classroom   C) laboratory   D) competition",
     answer: 0,
   },
   {
     type: "text",
-    question: "Solar panels make electricity ___ sunlight.",
-    answers: "A) from snow   B) at night   C) from sunlight   D) from water",
+    question:
+      "Fireworks become different colors because of special ________ inside them.",
+    answers: "A) gases   B) metals   C) metal salts   D) liquids",
+    answer: 2,
+  },
+  {
+    type: "text",
+    question: "My brother ________ going to build a model rocket this weekend.",
+    answers: "A) are   B) is   C) am   D) be",
+    answer: 1,
+  },
+  {
+    type: "text",
+    question:
+      "We ________ going to visit the science museum tomorrow because it is closed.",
+    answers: "A) are not   B) don’t   C) not are   D) isn’t",
+    answer: 0,
+  },
+  {
+    type: "text",
+    question: "________ they going to watch the fireworks tonight?",
+    answers: "A) Is   B) Am   C) Are   D) Do",
     answer: 2,
   },
   {
     type: "text",
     question:
-      "Sara likes reading books. She used to borrow books from the library every week. Now she usually reads e-books on her tablet because it is faster and easier. She still likes real books, but reading e-books is more convenient for her. Why does Sara usually read e-books now?",
+      "– Where are you going to spend your summer holiday?\n– We ________ going to visit Samarkand.",
+    answers: "A) is   B) are   C) am   D) be",
+    answer: 1,
+  },
+  {
+    type: "text",
+    question: "Choose the INCORRECT sentence.",
     answers:
-      "A) They are slower than real books   B) They are heavy to carry   C) They are faster and easier   D) They are less interesting",
+      "A) She is going to buy a new backpack.   B) They are going to travel by train.   C) We going to play basketball later.   D) I am not going to stay at home.",
     answer: 2,
   },
   {
     type: "text",
-    question:
-      "Many people ride bicycles every day. Bicycles are faster than walking, they do not make air dirty, and they help people stay healthy. Why do people ride bicycles?",
+    question: "The girl ________ won the art competition is my cousin.",
+    answers: "A) which   B) where   C) who   D) when",
+    answer: 2,
+  },
+  {
+    type: "text",
+    question: "This is the library ________ we usually study after school.",
+    answers: "A) who   B) where   C) which   D) what",
+    answer: 1,
+  },
+  {
+    type: "text",
+    question: "Match the sentence halves.\nThe movie was exciting, ...",
     answers:
-      "A) To travel faster and stay healthy   B) To make noise   C) To spend more money   D) To make the road bigger",
+      "A) but it was too long.   B) who helped me yesterday.   C) where we bought snacks.   D) which is my teacher.",
     answer: 0,
+  },
+  {
+    type: "text",
+    question: "I wanted to go swimming, ________ the water was too cold.",
+    answers: "A) so   B) because   C) but   D) and",
+    answer: 2,
+  },
+  {
+    type: "text",
+    question: "Choose the sentence where “and” is used correctly.",
+    answers:
+      "A) She opened the book and started reading.   B) She was hungry and she ate because.   C) He ran fast, and but he lost.   D) They played football and tired.",
+    answer: 0,
+  },
+  {
+    type: "text",
+    question:
+      "Why did Ben enjoy the science fair?\n\nHello Tom,\nYesterday our school had a science fair. Students made robots, volcanoes, and space models. My favorite project was a small robot that could carry books. I also liked the planet models because they looked very realistic. At the end of the fair, our class won first prize, and everyone cheered loudly. I was very proud and excited.\n\nSee you soon,\nBen",
+    answers:
+      "A) Because he stayed home all day.   B) Because he saw interesting projects and his class won.   C) Because he didn’t like the robots.   D) Because the fair was boring.",
+    answer: 1,
+  },
+  {
+    type: "text",
+    question:
+      "Why is Mars colder than Earth?\n\nMars and Earth are planets in our solar system. Earth is closer to the Sun than Mars. Because Mars is farther away, it receives less heat and light from the Sun. Scientists study Mars carefully because they want to learn if people can live there in the future.",
+    answers:
+      "A) Because Mars is bigger than Earth.   B) Because Mars has more oceans.   C) Because Mars is farther from the Sun.   D) Because Mars is closer to the Moon.",
+    answer: 2,
   },
 ];
+englishQuestions = englishQuestions.sort(() => Math.random() - 0.5);
 questions = [...mathQuestions, ...englishQuestions];
-
-// questions = questions.sort(() => Math.random() - 0.5);
 
 const questionImage = document.getElementById("questionImage");
 const options = document.querySelectorAll("input[name='answer']");
@@ -171,7 +177,6 @@ let text = document.getElementById("questionText");
 let englishAnswers = document.getElementById("englishAnswers");
 
 function loadQuestion() {
-  // questionImage.src = questions[currentQuestion].img;
   let q = questions[currentQuestion];
 
   if (q.type === "img") {
