6e65e077c25233ed5e662f1e53ce53186b062973
savbatovasadbek <asavbatov@gmail.com>
2026-05-18 16:40:15 +0500

    4-CHSB added

--- script.js
+++ script.js
@@ -25,7 +25,31 @@ function createChart(id, correct, wrong) {
 }
 
 let mathCorrectAnswers = [
-  2, 3, 1, 2, 1, 3, 1, 2, 3, 0, 1, 3, 2, 1, 2, 3, 0, 2, 3, 0, 2, 1, 2, 3, 1,
+  2,
+  1,
+  3,
+  0,
+  0,
+  1,
+  2,
+  2,
+  3,
+  2,
+  1,
+  2,
+  3, // 1-13 gacha bo'lgan javoblar
+  2,
+  0,
+  2,
+  0,
+  3,
+  1,
+  0,
+  2,
+  1,
+  3,
+  1,
+  0, // 14-25 gacha bo'lgan javoblar
 ];
 
 let mathQuestions = [];
